533c0b3db72bbb7617acdee1b85a3b2f9b1d2a52
4m1Th <amithc38@gmail.com>
2021-05-01 13:29:35 +0530

    added window handling

--- src/test/java/CookieEyes/CY.java
+++ src/test/java/CookieEyes/CY.java
@@ -1,9 +1,11 @@
 package CookieEyes;
 
 import java.util.List;
+import java.util.Set;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import CommonLib.Browser;
@@ -59,22 +61,59 @@ public class CY {
 		
 	}
 	
+//	@Test(priority=3)
+//	public void de_select() throws InterruptedException {
+//		
+//		br.driver.navigate().to(url.consent_banner);
+//		Thread.sleep(4000);
+//		br.driver.findElement(By.xpath(log2.drop)).click();
+//		Thread.sleep(2000);
+//		WebElement elementdrop = br.driver.findElement(By.xpath(log2.drop));
+//		List<WebElement> dropdownvalues = br.driver.findElements(By.xpath(log2.drop));
+//		for(WebElement value:dropdownvalues)
+//		{ 
+//		  String pcvalues=value.getText();
+//		  System.out.println("value names" + pcvalues);
+//		 }
+//		
+//	}
+//	
 	@Test(priority=3)
-	public void de_select() throws InterruptedException {
-		
-		br.driver.navigate().to(url.consent_banner);
-		Thread.sleep(4000);
-		br.driver.findElement(By.xpath(log2.drop)).click();
-		Thread.sleep(2000);
-		WebElement elementdrop = br.driver.findElement(By.xpath(log2.drop));
-		List<WebElement> dropdownvalues = br.driver.findElements(By.xpath(log2.drop));
-		for(WebElement value:dropdownvalues)
-		{ 
-		  String pcvalues=value.getText();
-		  System.out.println("value names" + pcvalues);
-		 }
-		
-	}
-	
+	public void WindowHandling() throws Exception {
+        String parentWindow = br.driver.getWindowHandle();
+        System.out.println("tittle = "+br.driver.getTitle());
+        Thread.sleep(3000);
+        br.driver.findElement(By.xpath("//*[@id=\"sidebar\"]/ul/li[6]/a")).click();
+        Thread.sleep(3000);
+        Set<String> allWindowHandles = br.driver.getWindowHandles();
+        //Switching control to privacy policy window
+        for(String handles: allWindowHandles) {
+            if(!parentWindow.equals(handles)) {
+                br.driver.switchTo().window(handles);
+                System.out.println("window switched");
+                break;
+            }
+        }
+        String privacyPolicyWindowTittleActual = br.driver.getTitle();
+        System.out.println("tittle = "+ privacyPolicyWindowTittleActual );
+        String privacyPolicyWindowTittleExpected = "CookieYes | Privacy Policy Generator";
+        //verifying that driver's control switched to privacy policy window.
+        Assert.assertEquals(privacyPolicyWindowTittleActual, privacyPolicyWindowTittleExpected);
+
+        //switching control to main window
+        for(String handles: allWindowHandles) {
+            if(parentWindow.equals(handles)) {
+                br.driver.switchTo().window(handles);
+                System.out.println("window switched");
+                break;
+            }
+        }
+        privacyPolicyWindowTittleActual = br.driver.getTitle();
+        System.out.println("tittle = "+ privacyPolicyWindowTittleActual );
+        privacyPolicyWindowTittleExpected = "CookieYes";
+        //verifying that we are in privacy policy window now.
+        Assert.assertEquals(privacyPolicyWindowTittleActual, privacyPolicyWindowTittleExpected);
+        //br.takeSnapShot(br.driver, input.screenshot1);
+    }
 
 }
